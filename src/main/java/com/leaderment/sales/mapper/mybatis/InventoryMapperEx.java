3bad7bad4719bef5b74693348b36773ab9fc38ac
Lee <851981471@qq.com>
2019-05-19 18:38:54 +0800

    git

---
+++ src/main/java/com/leaderment/sales/mapper/mybatis/InventoryMapperEx.java
@@ -0,0 +1,26 @@
+package com.leaderment.sales.mapper.mybatis;
+
+
+import com.leaderment.sales.pojo.ItemKey;
+
+import com.leaderment.sales.pojo.dto.ItemKeyDTO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface ItemKeyMapperEx {
+
+
+    List<ItemKeyDTO> findAll();
+
+
+    int getByItemKeyAndTypeAndBusinessUnitId(ItemKey itemKey);
+
+    List<ItemKey> findByBusinessUnitId(int businessUnitId);
+
+    int updateStatusByItemKeyId(@Param("status") int status, @Param("itemKeyId")int itemKeyId);
+
+    int updateItemKeyAndTypeByItemId(ItemKey itemKey);
+
+    List<ItemKey> findByUserIdAndType(@Param("userId")Integer userId,@Param("type")int type,@Param("status") int status);
+}
